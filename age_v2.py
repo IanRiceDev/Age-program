ff1f90867897e449fc7850878a94e7f9d4a3c5b0
IanRiceDev <ianriceitdeveloper@gmail.com>
2018-11-06 10:18:04 -0500

    Add age_v2.py
    Better PEP compliance and generally better designed.
    
    Need to comment agev2.py

---
+++ age_v2.py
@@ -0,0 +1,21 @@
+# Gets time
+import sys
+import datetime
+
+user_birth_year = input("Enter your year of birth:")
+
+get_year = int(datetime.datetime.now().year)
+
+test_for_digit = user_birth_year.isdigit()
+
+if test_for_digit is False:
+
+    print("That is an invalid number")
+    print("terminating program")
+    input()
+    sys.exit()
+
+elif test_for_digit is True:
+
+    test = get_year - int(user_birth_year)
+    print(test)
